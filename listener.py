2c2cf2458c2391a0901f04e7aeeae908a5a36284
Ryan <95035038+ryanv404@users.noreply.github.com>
2022-03-09 07:47:30 -0600

    formatting fixes

--- listener.py
+++ listener.py
@@ -1,5 +1,6 @@
+import os
 import logging
-import configparser
+from switches import get_chat_name, get_flag
 from googletrans import Translator
 from telethon import TelegramClient, events
 
@@ -10,67 +11,15 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # Create a translator instance
 translator = Translator()
 
-# Read config.ini
-config = configparser.ConfigParser()
-config.read("config.ini")
-
 # Set config values
-api_id = config['Telegram']['api_id']
-api_hash = config['Telegram']['api_hash']
-phone = config['Telegram']['phone']
-username = config['Telegram']['username']
-channel_link = config['Telegram']['channel_link']
+api_id = os.environ.get('api_id')
+api_hash = os.environ.get('api_hash')
+phone = os.environ.get('phone')
+username = os.environ.get('username')
+channel_link = os.environ.get('channel_link')
 
 # Create the client
 client = TelegramClient(username, api_id, api_hash)
-
-# Switch source language to the corresponding flag emoji
-def get_flag(src):
-    match src:
-            case 'en':
-                return "🇬🇧"
-            case 'uk':
-                return "🇺🇦"
-            case 'ru':
-                return "🇷🇺"
-            case _:
-                return "🇦🇶"
-
-# Mark state-affiliated media channels and format channel names
-def get_chat_name(chat):
-    match chat.username:
-        case 'tass_agency':
-            return 'TASS \n~~Russia state-affiliated media 🤨~~'
-        case 'MID_Russia':
-            return 'MID Russia \n~~Russia state-affiliated media 🤨~~'
-        case 'rian_ru':
-            return 'RIA Novosti \n~~Russia state-affiliated media 🤨~~'
-        case 'mvs_ukraine':
-            return 'MVS Ukraine \n~~Ukraine state-affiliated media 🤷‍♂️~~'
-        case 'insiderUKR':
-            return 'Insider Ukraine War News'
-        case 'uniannet':
-            return 'UNIAN - Ukraine News'
-        case 'voynareal':
-            return 'Real War News Ukraine'
-        case 'suspilnesumy':
-            return 'Public Sumy'
-        case 'milinfolive':
-            return 'Military Informant'
-        case 'KharkivPolitics':
-            return 'Kharkiv.Main.Politics'
-        case 'KyivPolitics':
-            return 'Kyiv.Main.Politics'
-        case 'faceofwar':
-            return 'Face of War'
-        case 'ukr_pravda':
-            return 'Ukraine Pravda'
-        case _:
-            if chat.title:
-                return chat.title
-            else:
-                return chat.username
-                
 
 # Listen for new messages
 @client.on(events.NewMessage)
@@ -81,13 +30,20 @@ async def handler(e):
     if content.text:
         chat = await e.get_chat()
         chat_name = get_chat_name(chat)
-        print(str(chat.id) + '/' + str(e.id))
 
-        link = f't.me/{chat.username}'
+        if chat.username:
+            link = f't.me/{chat.username}'
+        else:
+            link = f't.me/c/{chat.id}'
+
         flag = get_flag(content.src)
-        message = f'\"{flag}"  **[{chat_name}]({link})**\n\n{content.text}'
+        message_id = e.id
+        message = f'📣 \n\n\"{flag}" [{chat_name}]({link}) \n\n{content.text} \n\n[👁‍🗨]({link}/{message_id})'
 
-        await client.send_message(channel_link, message, link_preview=False)
+        try:
+            await client.send_message(channel_link, message, link_preview=False)
+        except:
+            print('[Telethon] Error while sending message!')
 
 # Connect client
 client.start()
---
+++ switches.py
@@ -0,0 +1,48 @@
+# Switch source language to the corresponding flag emoji
+def get_flag(src):
+    match src:
+            case 'en':
+                return "🇬🇧"
+            case 'uk':
+                return "🇺🇦"
+            case 'ru':
+                return "🇷🇺"
+            case _:
+                return "🇦🇶"
+
+# Mark state-affiliated media channels and format channel names
+def get_chat_name(chat):
+    match chat.username:
+        case 'tass_agency':
+            return 'TASS \nRussia state-affiliated media 🤨'
+        case 'MID_Russia':
+            return 'MID Russia \nRussia state-affiliated media 🤨'
+        case 'rian_ru':
+            return 'RIA Novosti \nRussia state-affiliated media 🤨'
+        case 'mvs_ukraine':
+            return 'MVS Ukraine \nUkraine state-affiliated media 🤷‍♂️'
+        case 'insiderUKR':
+            return 'Insider Ukraine War News'
+        case 'uniannet':
+            return 'UNIAN - Ukraine News'
+        case 'voynareal':
+            return 'Real War News Ukraine'
+        case 'suspilnesumy':
+            return 'Public Sumy'
+        case 'milinfolive':
+            return 'Military Informant'
+        case 'KharkivPolitics':
+            return 'Kharkiv.Main.Politics'
+        case 'KyivPolitics':
+            return 'Kyiv.Main.Politics'
+        case 'faceofwar':
+            return 'Face of War'
+        case 'ukr_pravda':
+            return 'Ukraine Pravda'
+        case 'a_shtirlitz':
+            return 'Anatoly Shtirlitz'
+        case _:
+            if chat.title:
+                return chat.title
+            else:
+                return chat.username
