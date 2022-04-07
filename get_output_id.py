fe34ad6ea694e523abb5f0e16a93c3da50d819c1
Ryan <95035038+ryanv404@users.noreply.github.com>
2022-04-07 15:34:46 -0500

    added video listener

--- get_output_id.py
+++ get_output_id.py
@@ -6,7 +6,10 @@ async def start(config):
     client = TelegramClient(config["session_name"], config["api_id"], config["api_hash"])
     await client.start()
     print('OUTPUT CHANNEL ID:')
+    print('TELEHUNT:')
     print(str((await client.get_entity('https://t.me/telehunt_video')).id))
+    print('RYAN_TEST_CHANNEL:')
+    print(str((await client.get_entity('https://t.me/ryan_test_channel')).id))
     await client.disconnect()
 
 if __name__ == "__main__":
