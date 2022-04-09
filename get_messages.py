b6f3f6b05d14effc5d60ecec9b4267db3d017ff5
Ryan <95035038+ryanv404@users.noreply.github.com>
2022-04-09 02:07:44 -0500

    added get_messages

---
+++ get_messages.py
@@ -0,0 +1,43 @@
+# t.me/opersvodki/3054
+# for some reason a link to t.me/opersvodki/3051 was inserted at the '4'
+
+from telethon import TelegramClient
+from googletrans import Translator
+import yaml
+
+with open('config.yml', 'rb') as f:
+    config = yaml.safe_load(f)
+
+# Create the client
+client = TelegramClient(config["session_name"], config["api_id"], config["api_hash"])
+
+# Connect client
+client.start()
+
+# Create a translator instance
+translator = Translator()
+
+# Get messages
+message_num = 1
+channel_username = 'opersvodki'
+for message in client.get_messages(channel_username, limit=3):
+    print(f'MESSAGE NUMBER {message_num}:')
+    print(message.message)
+    message_num += 1
+
+# Translate with Google Translator (source language is auto-detected; output language is English)
+# content = translator.translate(e.message.message)
+# text = content.text
+
+# chat = await e.get_chat()
+# if chat.username:
+#     link = f't.me/{chat.username}'
+# else:
+#     link = f't.me/c/{chat.id}'
+
+# message_id = e.id
+# border = '~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~'
+# message = f'{link}/{message_id} ↩\n\n{border}\n{chat.title}\n{border}\n\n[ORIGINAL MESSAGE]\n{e.message.message}\n\n[TRANSLATED MESSAGE]\n{text}'
+
+# Run client until a keyboard interrupt (ctrl+C)
+client.run_until_disconnected()
