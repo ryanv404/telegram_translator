6461d78396e45d388d41192254b22462464ef3b9
Ryan <95035038+ryanv404@users.noreply.github.com>
2022-04-10 01:21:37 -0500

    updated get_messages

--- get_messages.py
+++ get_messages.py
@@ -33,43 +33,55 @@ all_messages = []
 total_messages = 0
 total_count_limit = 0
 
-while True:
-    print("Current Offset ID is:", offset_id, "; Total Messages:", total_messages)
-    history = client(GetHistoryRequest(
-        peer="https://t.me/UkrRusWarNews",
-        offset_id=offset_id,
-        offset_date=None,
-        add_offset=0,
-        limit=limit,
-        max_id=0,
-        min_id=0,
-        hash=0
-    ))
+# while True:
+#     print("Current Offset ID is:", offset_id, "; Total Messages:", total_messages)
+#     history = client(GetHistoryRequest(
+#         peer="https://t.me/UkrRusWarNews",
+#         offset_id=offset_id,
+#         offset_date=None,
+#         add_offset=0,
+#         limit=limit,
+#         max_id=0,
+#         min_id=0,
+#         hash=0
+#     ))
 
-    if not history.messages:
-        break
+#     if not history.messages:
+#         break
     
-    messages = history.messages
-    for message in messages:
-        all_messages.append(message.to_dict())
+#     messages = history.messages
+#     for message in messages:
+#         all_messages.append(message.to_dict())
     
-    offset_id = messages[len(messages) - 1].id
+#     offset_id = messages[len(messages) - 1].id
     
-    total_messages = len(all_messages)
-    if total_count_limit != 0 and total_messages >= total_count_limit:
-        break
+#     total_messages = len(all_messages)
+#     if total_count_limit != 0 and total_messages >= total_count_limit:
+#         break
 
-print(f'There are {total_messages} total messages found.')
+# print(f'There are {total_messages} total messages found.')
 
-count = 1
-if total_messages >= 1:
-    for message in all_messages:
-        if re.search('.*filtration camp[s]?.*', message.message, re.IGNORECASE):
-            print('')
-            print(message.message)
-            count += 1
 
-print(f'{count} messages matched "filtration camp(s)"')
+total_count = 0
+search_count = 0
+message_list = []
+for message in client.iter_messages('UkrRusWarNews'):
+    total_count += 1
+    if (type(message.message) is str) and (re.search('.*filtration camp[s]?.*', message.message, re.IGNORECASE)):
+        search_count += 1
+        print(search_count)
+        message_list.append(message.message)
+        
+print(f'{search_count} from {total_count} messages')
+
+# if total_messages >= 1:
+#     for message in all_messages:
+#         if re.search('.*filtration camp[s]?.*', message.message, re.IGNORECASE):
+#             print('')
+#             print(message.message)
+#             count += 1
+
+# print(f'{count} messages matched "filtration camp(s)"')
 
 # Run client until a keyboard interrupt (ctrl+C)
 client.run_until_disconnected()
