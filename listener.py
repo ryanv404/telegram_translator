e57092d8e658d0905e575d3c13ac42cc70330595
Ryan <95035038+ryanv404@users.noreply.github.com>
2022-04-11 12:56:36 -0500

    added photo listener

--- listener.py
+++ listener.py
@@ -106,6 +106,7 @@ async def handler(e):
                 f'<p>{border}\n'
                 f'{link}/{message_id} ↩</p></p>') 
 
+        print(e)
         if chat.username not in ['photo_posts', 'shadedPineapple', 'my_search_results', 'video_posts', 'ukr_rus_war_news', 'cyberbenb', 'Telegram']:
             if eng_search_terms_present(translated_msg) or ru_search_terms_present(e.message.message): 
                 try:
